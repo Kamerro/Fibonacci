72d6c5d90d71bb5675351edd91b22c253abadbae
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-06-29 14:02:26 +0200

    Dodaj pliki projektów.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿//Here you can chceck the result of the fucntions!
+
+
+Console.WriteLine(CalcFibonacciIterationalWay(8)); //should be 21
+Console.WriteLine(CalcFibonacciWithRecursion(8)); //should be 21
+
+int CalcFibonacciIterationalWay(int position)
+{
+    int value1 = 1;
+    int value2 = 1;
+    for(int i = 2; i < position; i++)
+    {
+        value1 += value2;
+        value2 = value1 - value2;
+    }
+    return value1;
+}
+int CalcFibonacciWithRecursion(int position)
+{
+    if(position ==1 || position == 2)
+    {
+        return 1;
+    }
+    return CalcFibonacciWithRecursion(position - 1) + CalcFibonacciWithRecursion(position -2);
+}
